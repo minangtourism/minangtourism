1d7246da0f7d7e73ba5ce6a5166f323b3e9ebcda
amin <informatika_amin@yahoo.co.id>
2012-07-28 22:21:06 +0700

    menghapus image dan membuat seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120727190402) do
+ActiveRecord::Schema.define(:version => 20120727190310) do
 
   create_table "category_loc_tourisms", :force => true do |t|
     t.string   "name"
@@ -277,10 +277,6 @@ ActiveRecord::Schema.define(:version => 20120727190402) do
     t.integer  "tourism_article_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "tourism_articles", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 dummy_images = {
-  :image_tourism_article => (open(Rails.root.join("app/assets/images/tourism_article.jpg")) rescue nil),
-  :image_folktale => (open(Rails.root.join("app/assets/images/acara_wisata.jpg")) rescue nil),
-  :image_tips_trick => (open(Rails.root.join("app/assets/images/tips_trik_wisata.jpg")) rescue nil),
-  :image_event => (open(Rails.root.join("app/assets/images/acara.jpg")) rescue nil)
+  :image_event => (open(Rails.root.join("app/assets/images/acara_wisata.jpg")) rescue nil),
+  :image_folktale => (open(Rails.root.join("app/assets/images/cerita_rakyat.jpg")) rescue nil),
+  :image_location_tourism => (open(Rails.root.join("app/assets/images/tempat_wisata.jpg")) rescue nil),
+  :image_slideshow => (open(Rails.root.join("app/assets/images/slideshow.jpg")) rescue nil),
+  :image_tips_trick => (open(Rails.root.join("app/assets/images/tips_wisata.jpg")) rescue nil),
+  :image_tourism_article => (open(Rails.root.join("app/assets/images/berita_wisata.jpg")) rescue nil)
 }
+
+#Setting.destroy_all
+#
+#Setting.create(
+#  key: Faker::Lorem.sentence.gsub(/\.$/, ""),
+#  alt: Faker::Lorem.sentence.gsub(/\.$/, ""),
+#  value: Faker::Lorem.sentence.gsub(/\.$/, "")
+#)
 
 User.destroy_all #menghapus user, jika ditambah data user baru id:integer, id = 1, (rake db:reset)
 User.valid_roles.each do |role|
@@ -31,39 +41,11 @@ members = User.all.select do |user|
   user.is? :member
 end
 
-TourismArticle.destroy_all
+CategoryLocTourism.destroy_all
 
-1.upto(15).each do |i|
-  member = members[i.pred % members.count]
-  member.tourism_articles.create!(
-    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
-    content: Faker::Lorem.paragraph(6),
-    image: dummy_images[:image_tourism_article],
-    state: "unpublished"
-  )
-end
-
-Folktale.destroy_all
-
-1.upto(15).each do |i|
-  member = members[i.pred % members.count]
-  member.folktales.create!(
-    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
-    description: Faker::Lorem.paragraph(6),
-    image: dummy_images[:image_folktale],
-    state: "unpublished"
-  )
-end
-
-TipsTrick.destroy_all
-
-1.upto(15).each do |i|
-  member = members[i.pred % members.count]
-  member.tips_tricks.create!(
-    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
-    description: Faker::Lorem.paragraph(6),
-    image: dummy_images[:image_tips_trick],
-    state: "unpublished"
+1.upto(5).each do
+  CategoryLocTourism.create(
+    name: Faker::Lorem.sentence.gsub(/\.$/, "")
   )
 end
 
@@ -77,6 +59,74 @@ Event.destroy_all
     start_date: "2012-01-01".to_date,
     end_date: "2012-02-01".to_date,
     image: dummy_images[:image_event],
-    state: "unpublished"
+    state: "published"
+  )
+end
+
+Folktale.destroy_all
+
+1.upto(15).each do |i|
+  member = members[i.pred % members.count]
+  member.folktales.create!(
+    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    description: Faker::Lorem.paragraph(6),
+    image: dummy_images[:image_folktale],
+    state: "published"
+  )
+end
+
+LocationTourism.destroy_all
+
+1.upto(15).each do |i|
+  member = members[i.pred % members.count]
+  member.location_tourisms.create!(
+    name: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    category_loc_tourism_id: "1",
+    address: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    city: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    phone: "1+ (123) 456.4789",
+    web: "www.nama-web.com",
+    facebook: "www.facebook.com",
+    twitter: "www.twitter.com",
+    hours_description: Faker::Lorem.paragraph(1),
+    facility: Faker::Lorem.paragraph(1),
+    image: dummy_images[:image_location_tourism],
+    description: Faker::Lorem.paragraph(6),
+    state: "published"
+  )
+end
+
+Slideshow.destroy_all
+
+1.upto(5).each do |i|
+  Slideshow.create(
+    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    description: Faker::Lorem.paragraph(1),
+    image: dummy_images[:image_slideshow],
+    state: "published"
+  )
+end
+
+TipsTrick.destroy_all
+
+1.upto(15).each do |i|
+  member = members[i.pred % members.count]
+  member.tips_tricks.create!(
+    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    description: Faker::Lorem.paragraph(6),
+    image: dummy_images[:image_tips_trick],
+    state: "published"
+  )
+end
+
+TourismArticle.destroy_all
+
+1.upto(15).each do |i|
+  member = members[i.pred % members.count]
+  member.tourism_articles.create!(
+    title: Faker::Lorem.sentence.gsub(/\.$/, ""),
+    content: Faker::Lorem.paragraph(6),
+    image: dummy_images[:image_tourism_article],
+    state: "published"
   )
 end
